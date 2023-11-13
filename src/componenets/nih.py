6f842d4ec5ee95749d44998537f48da88926eb45
dongseopkim <dongseop.kim@vuno.co>
2023-11-14 01:44:43 +0900

    update componenets nih

--- src/componenets/nih.py
+++ src/componenets/nih.py
@@ -1,12 +1,29 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional,List, Union, Tuple
 from pathlib import Path
 
 import cv2
+import torch
+import numpy as np
 from torch.utils.data import Dataset
 
 from ..utils.common import get_repo_root, load_json
 
 
+_MAPPER = {'effusion': 1,
+           'emphysema': 2,
+           'atelectasis': 3,
+           'edema': 4,
+           'consolidation': 5, 
+           'pleural_thickening': 6,
+           'hernia': 7,
+           'mass': 8,
+           'nofinding': 9,
+           'cardiomegaly': 10,
+           'nodule': 11,
+           'pneumothorax': 12,
+           'pneumonia': 13,
+           'fibrosis': 14,
+           'infiltration': 15}
 
 class NIH(Dataset):
     """ NIH Dataset
@@ -15,6 +32,7 @@ class NIH(Dataset):
         root_dir (string): Path to the root directory of the dataset.
         split: (strinf) : train, test, or valid
         transform (callable, optional): Optional transform to be applied on a sample.
+        image_channels (int): 1 for grayscale, 3 for RGB
         
     Returns:
         Dict: A dictionary containing the image, its labels, follow-up number, patient ID, patient age, patient gender, 
@@ -25,11 +43,13 @@ class NIH(Dataset):
                  root_dir:str, 
                  split:str='train',
                  transform:Optional[Dict[str,Any]]=None,
+                 image_size:Union[int, Tuple[int,int]]=(1024,1024),
                  image_channels:int=3,
                  **kwargs):
         self.root_dir = root_dir
         self.split = split
         self.transform = transform
+        self.image_size = image_size if isinstance(image_size, tuple) else (image_size, image_size)
         self.image_channels = image_channels
 
         self.annots = self._load_annotations()
@@ -37,16 +57,30 @@ class NIH(Dataset):
 
     def __getitem__(self, index:int) -> Dict[str, Any]:
         annot = self._load_data(index)
-        
-        return annot
+        image: np.ndarray = annot['image'] # (H,W,3 or 1)
+        path:str = annot['path']
+        labels:List[int] = annot['labels']
 
-    def __len__(self):
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed['image']
+        
+        image = torch.Tensor(image).permute(2,0,1) # (H,W,3 or 1) -> (3 or 1, H, W)
+        labels = torch.Tensor(labels).long()
+
+        return {'image':image,
+                'label': labels,
+                'path': path}
+
+    def __len__(self) -> int:
         return len(self.annots)
 
 
     def _load_annotations(self):
         path = Path(get_repo_root()) / 'data/nih.zip'
-        return load_json(path)
+        annots = load_json(path)
+        return [annot for annot in annots if annot['split'] == self.split]
+
     
     def _load_data(self, idx) -> Dict[str, Any]:
         annot = self.annots[idx]
@@ -58,17 +92,30 @@ class NIH(Dataset):
         label_indexes = annot['label_indexes']
 
         image_path = Path(self.root_dir) / filename
-        image = cv2.imread(str(image_path))
-        image = cv2.cvtColor(image, cv2.IMREAD_GRAYSCALE)
-        if self.image_channels == 3:
-            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        image = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
+        
+        # resize to 1024 x 1024
+        image = cv2.resize(image, self.image_size, interpolation=cv2.INTER_LINEAR)
+        
+        if len(image.shape) == 2:
+            image = np.expand_dims(image, axis=2) # (H,W) -> (H,W,1)
+    
+        if self.image_channels == 3 and image.shape[2] == 1: 
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB) # (H,W,1) -> (H,W,3)
+
+        # min-max normalization
+        image = image.astype(np.float32)
+        image = (image - np.min(image)) / (np.max(image) - np.min(image))
+        image = image * 255.0
+        image = image.astype(np.uint8)
+
         return {'image':image,
                 'path': image_path,
                 'gender': gender, 
                 'age': age,
                 'view_position': view_position,
                 'label_names': label_names,
-                'label_indexes': label_indexes}
+                'labels': label_indexes}
 
 
         
--- src/componenets/nih_test.py
+++ src/componenets/nih_test.py
@@ -1,16 +1,51 @@
 import pytest
 import logging
+from pathlib import Path
+
+import numpy as np
 
 from .nih import NIH
+from ..utils.common import get_repo_root
 
 logger = logging.getLogger(__name__)
 
 @pytest.fixture
-def nih():
+def nih_train():
     return NIH(root_dir='', split='train')
 
-def test_nih_len(nih:NIH):
-    assert len(nih) == 112120, "The length of the dataset should be 112120"
-    logger.debug(nih.annots[0])
+@pytest.fixture
+def nih_test():
+    return NIH(root_dir='', split='test')
+
+@pytest.fixture
+def nih_val():
+    return NIH(root_dir='', split='val')
+
+def test_nih_train_len(nih_train:NIH):
+    assert len(nih_train) == 89783, "The length of the dataset should be 89783"
+    assert nih_train.annots[0]['split'] == 'train', "The split should be train"
     return 
 
+def test_nih_test_len(nih_test:NIH):
+    assert len(nih_test) == 11219, "The length of the dataset should be 11219"
+    assert nih_test.annots[0]['split'] == 'test', "The split should be test"
+    return
+
+def test_nih_val_len(nih_val:NIH):
+    assert len(nih_val) == 11118, "The length of the dataset should be 11118"
+    assert nih_val.annots[0]['split'] == 'val', "The split should be test"
+    return
+
+def test_nih_load_data(nih_train:NIH):
+    nih_train.root_dir = Path(get_repo_root()) / 'data'
+    nih_train.image_channels = 3
+    dummy = nih_train._load_data(0)
+    image:np.ndarray = dummy['image']
+    assert image.shape == (1024, 1024, 3), "The image should be 1024 x 1024 x 3"
+    assert image.max() == 255.0, "The image should be normalized to 255.0"
+    assert image.min() == 0.0, "The image should be normalized to 0.0"
+
+    nih_train.image_channels = 1
+    dummy = nih_train._load_data(0)
+    image:np.ndarray = dummy['image']
+    assert image.shape == (1024, 1024, 1), "The image should be 1024 x 1024 x 3"
