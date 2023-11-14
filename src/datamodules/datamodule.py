314b1c40a08f2bd3f7584560d02e8ccd6089728c
dongseopkim <dongseop.kim@vuno.co>
2023-11-14 15:22:03 +0900

    update examples

--- src/datamodules/datamodule.py
+++ src/datamodules/datamodule.py
@@ -1,1 +1,6 @@
-from torch.utils.data import Dataset
+from functools import partial
+
+from torch.utils.data import DataLoader
+
+
+DataLoaderNIHTrain = partial(DataLoader, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
