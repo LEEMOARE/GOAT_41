e57d80e8c8a92a05aea687a9834ed1914da140d2
Dongseop Kim / 김동섭 <96169994+dongseop-kim@users.noreply.github.com>
2023-11-14 14:59:05 +0900

    Update components nih (#10)
    * Modify NIH dataset code and add new Python file (#5)
    
    * Modify NIH dataset code and add new Python file
    
    * Update README.md
    
    * nih.py Update
    
    * Refactor repo (#6)
    
    * update gitignore
    
    * remove python cache files
    
    * remove nih csv file in components
    
    * rename test file
    
    * rename test file
    
    * update age distribution (#7)
    
    ---------
    
    Co-authored-by: wonnypark <116738002+wonnypark@users.noreply.github.com>
    Co-authored-by: Dongseop Kim / 김동섭 <96169994+dongseop-kim@users.noreply.github.com>
    
    * update gitignore
    
    * wip
    
    * update componenets nih
    
    * update doc string
    
    * wip
    
    ---------
    
    Co-authored-by: mocha <136784736+mocha-Bread@users.noreply.github.com>
    Co-authored-by: wonnypark <116738002+wonnypark@users.noreply.github.com>

--- src/componenets/nih.py
+++ src/componenets/nih.py
@@ -1,96 +1,126 @@
-import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional,List, Union, Tuple
+from pathlib import Path
+
 import cv2
+import torch
+import numpy as np
 from torch.utils.data import Dataset
-from torchvision import transforms
-from PIL import Image
 
-_MAPPER = {'Effusion': 1,
-           'Emphysema': 2,
-           'Atelectasis': 3,
-           'Edema': 4,
-           'Consolidation': 5, 
-           'Pleural_Thickening': 6,
-           'Hernia': 7,
-           'Mass': 8,
-           'No Finding': 9,
-           'Cardiomegaly': 10,
-           'Nodule': 11,
-           'Pneumothorax': 12,
-           'Pneumonia': 13,
-           'Fibrosis': 14,
-           'Infiltration': 15}
+from ..utils.common import get_repo_root, load_json
 
+
+_MAPPER = {'effusion': 1,
+           'emphysema': 2,
+           'atelectasis': 3,
+           'edema': 4,
+           'consolidation': 5, 
+           'pleural_thickening': 6,
+           'hernia': 7,
+           'mass': 8,
+           'nofinding': 9,
+           'cardiomegaly': 10,
+           'nodule': 11,
+           'pneumothorax': 12,
+           'pneumonia': 13,
+           'fibrosis': 14,
+           'infiltration': 15}
 
 class NIH(Dataset):
     """ NIH Dataset
 
     Args:
-        dataframe (pandas.DataFrame): A DataFrame containing the annotations. Each row in the DataFrame represents an image and contains information such as the patient ID and the labels.
         root_dir (string): Path to the root directory of the dataset.
-        label_mapping (Dict): A dictionary mapping the labels in the dataframe to the desired output labels.
-        transform (callable, optional): Optional transform to be applied on a sample.
+        split: (string) : train, test, or valid
+        transform (Dict[str, Any]): A dictionary containing the image transformation.
+        image_size (int or tuple): The size of the image. If int, a square image is returned. If tuple, 
+            the image is resized to the size specified by the tuple.
+        image_channels (int): 1 for grayscale, 3 for RGB
         
     Returns:
         Dict: A dictionary containing the image, its labels, follow-up number, patient ID, patient age, patient gender, 
         view position, original image width, original image height, and original image pixel spacing. 
     """
     
-    def __init__(self, dataframe:str, root_dir:str,label_mapping, transform:Optional[Dict[str,Any]]=None,split='train'):
+    def __init__(self, 
+                 root_dir:str, 
+                 split:str='train',
+                 transform:Optional[Dict[str,Any]]=None,
+                 image_size:Union[int, Tuple[int,int]]=(1024,1024),
+                 image_channels:int=3,
+                 **kwargs):
         self.root_dir = root_dir
-        self.dataframe = dataframe
         self.split = split
-        
-        if transform: 
-            self.transform = transform 
-        else: 
-            self.transform = transforms.Compose([
-                transforms.Resize((256, 256)),  # 이미지 크기를 256x256으로 조정
-                transforms.ToTensor()
-            ])
-        self.label_mapping = label_mapping
+        self.transform = transform
+        self.image_size = image_size if isinstance(image_size, tuple) else (image_size, image_size)
+        self.image_channels = image_channels
+
+        self.annots = self._load_annotations()
        
 
     def __getitem__(self, index:int) -> Dict[str, Any]:
-        row = self.dataframe.iloc[index]
-        image_path = os.path.join(self.root_dir, row['Image Index'])
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)  
-       
+        annot = self._load_data(index)
+        image: np.ndarray = annot['image'] # (H,W,3 or 1)
+        path:str = annot['path']
+        labels:List[int] = annot['labels']
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed['image']
+        
+        image = torch.Tensor(image).permute(2,0,1) # (H,W,3 or 1) -> (3 or 1, H, W)
+        labels = torch.Tensor(labels).long()
+
+        return {'image':image,
+                'label': labels,
+                'path': path}
+
+    def __len__(self) -> int:
+        return len(self.annots)
 
 
-        if self.transform:
-            image = self.transform(image)
-        labels = row['Finding Labels'].split('|')
-        mapped_labels = [self.label_mapping[label] for label in labels]#
-        follow_up = row['Follow-up #']
-        patient_id = row['Patient ID']
-        Patient_Age = row['Patient Age']
-        Patient_Gender = row['Patient Gender']
-        View_Position = row['View Position']
-        original_image_width = row['OriginalImage[Width']
-        original_image_height = row['Height]']
-        original_image_pixel_spacing_x = row['OriginalImagePixelSpacing[x']
-        original_image_pixel_spacing_y = row['y]']
+    def _load_annotations(self):
+        path = Path(get_repo_root()) / 'data/nih.zip'
+        annots = load_json(path)
+        return [annot for annot in annots if annot['split'] == self.split]
 
-        return {
-        'image': image, 
-        'label': mapped_labels,
-        'split': self.split,
-        'follow_up': follow_up, 
-        'patient_id': patient_id,
-        'patient_age': Patient_Age,
-        'patient_gender': Patient_Gender,
-        'view_position': View_Position,
-        'original_image_width': original_image_width,
-        'original_image_height': original_image_height,
-        'original_image_pixel_spacing_x': original_image_pixel_spacing_x,
-        'original_image_pixel_spacing_y': original_image_pixel_spacing_y,
-    }
     
-    def __len__(self):
-        return len(self.dataframe)
+    def _load_data(self, idx) -> Dict[str, Any]:
+        annot = self.annots[idx]
+        filename = annot['filename']
+        gender = annot['gender']
+        age = annot['age']
+        view_position = annot['view_position']
+        label_names = annot['label_names']
+        label_indexes = annot['label_indexes']
+
+        image_path = Path(self.root_dir) / filename
+        image = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
+        
+        image = cv2.resize(image, self.image_size, interpolation=cv2.INTER_LINEAR)
+        
+        if len(image.shape) == 2:
+            image = np.expand_dims(image, axis=2) # (H,W) -> (H,W,1)
+    
+        if self.image_channels == 3 and image.shape[2] == 1: 
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB) # (H,W,1) -> (H,W,3)
+
+        # min-max normalization
+        image = image.astype(np.float32)
+        image = (image - np.min(image)) / (np.max(image) - np.min(image))
+        # to 8bit image 
+        image = image * 255.0
+        image = image.astype(np.uint8)
+
+        return {'image':image,
+                'path': image_path,
+                'gender': gender, 
+                'age': age,
+                'view_position': view_position,
+                'label_names': label_names,
+                'labels': label_indexes}
+
+
+        
 
 
     
