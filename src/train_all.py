2ec07d150a1091b49ac7adfcf89b1f17b5fd7432
mocha <qwert1209787@gmail.com>
2023-11-23 17:44:24 +0900

    wip

--- src/train_all.py
+++ src/train_all.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn as nn
 from torchmetrics import Recall, Specificity
 import os
-from .componenets.nih import NIH
-from .datamodules.datamodule import get_NIH_dataloader
-from .models.resnet import NIHResNet
+from src.componenets.nih import NIH
+from src.datamodules.datamodule import get_NIH_dataloader
+from src.models.resnet import NIHResNet
+
 
 
 def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', device: int = 0, max_epoch: int = 100):
@@ -16,8 +17,8 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
                    image_size=512, image_channels=3)
     testset = NIH(root_dir=root_dir, split='test',
                   image_size=512, image_channels=3)
-    validset = NIH(root_dir=root_dir, split='valid',
-                   image_size=512, image_channels=3)
+    validset = NIH(root_dir=root_dir, split='val',
+                   image_size=512, image_channels=3,transform=None)
 
     num_classes = trainset.num_classes
 
@@ -49,7 +50,7 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
     
 
 
-    for num_epoch in range(max_epoch):  
+    for num_epoch in range(max_epoch):  # epoch
         for num_iter, batch in enumerate(loader_train):
             image: torch.Tensor = batch['image']
             labels: torch.Tensor = batch['label']
@@ -81,45 +82,54 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
                 print(["{:.6f}".format(sens) for sens in computed_sens])
                 print(["{:.6f}".format(spec) for spec in computed_spec])
         model.eval()
+        best_accuracy = 0.0  # 초기 최고 정확도
         with torch.no_grad():
-                correct = 0
-                total = 0
-                for num_iter, batch in enumerate(loader_valid):
-                    image: torch.Tensor = batch['image']
-                    labels: torch.Tensor = batch['label']
-                    image = image.to(device)
-                    labels = labels.to(device)
-                    outputs = model(image)
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
+            correct = 0
+            total = 0
+            for num_iter, batch in enumerate(loader_valid):
+                image: torch.Tensor = batch['image']
+                labels: torch.Tensor = batch['label']
+                image = image.to(device)
+                labels = labels.to(device)
+                logits = model(image)
+                probs = torch.sigmoid(logits)  # Apply sigmoid to the logits
+                predicted = (probs.ge(0.4)).float()  # Generate predictions based on the threshold
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-                    for idx_lesion in range(num_classes):
-                        sens_per_lesion[idx_lesion].update(
-                            outputs[:, idx_lesion], labels[:, idx_lesion])
-                        spec_per_lesion[idx_lesion].update(
-                            outputs[:, idx_lesion], labels[:, idx_lesion])
+                print(f'Outputs for iteration {num_iter}: {probs}')  # Use probs instead of outputs
 
-                    computed_sens = [sens_per_lesion[idx_lesion].compute().item()
-                                    for idx_lesion in range(num_classes)]
-                    computed_spec = [spec_per_lesion[idx_lesion].compute().item()
-                                    for idx_lesion in range(num_classes)]
+                for idx_lesion in range(num_classes):
+                    sens_per_lesion[idx_lesion].update(
+                        probs[:, idx_lesion], labels[:, idx_lesion])  # Use probs instead of outputs
+                    spec_per_lesion[idx_lesion].update(
+                        probs[:, idx_lesion], labels[:, idx_lesion])  # Use probs instead of outputs
 
-                    if num_iter % 10 == 0:
-                        print(
-                            f'validation iteration {num_iter:5d} {num_iter/len(loader_valid)*100:.0f}')
-                        print(["{:.6f}".format(sens) for sens in computed_sens])
-                        print(["{:.6f}".format(spec) for spec in computed_spec])
+                computed_sens = [sens_per_lesion[idx_lesion].compute().item()
+                                for idx_lesion in range(num_classes)]
+                computed_spec = [spec_per_lesion[idx_lesion].compute().item()
+                                for idx_lesion in range(num_classes)]
 
-                if total > 0:
-                    accuracy = correct / total
-                    if accuracy > best_accuracy:
-                        best_accuracy = accuracy
-                        torch.save(model.state_dict(), os.path.join(root_dir2, 'best_model1.pth'))
-                else:
-                    print("Warning: No valid data found.")
+                # Print additional information for each 10 iterations
+                if num_iter % 10 == 0:
+                    print(
+                        f'Validation iteration {num_iter:5d} {num_iter/len(loader_valid)*100:.0f}')
+                    print(f'Predicted: {predicted}')
+                    print(f'Labels: {labels}')
+                    print(["{:.6f}".format(sens) for sens in computed_sens])
+                    print(["{:.6f}".format(spec) for spec in computed_spec])
+                    print('')
 
-        model.train()
-            
-        scheduler.step()
+            accuracy = correct / total
+            print(f'Accuracy on validation set: {accuracy:.4f}')
+
+            # 모델 저장
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                model_path = os.path.join(root_dir2, 'best_model.pth')
+                torch.save(model.state_dict(), model_path)
+                print('Best model saved with accuracy {:.4f}'.format(best_accuracy))
+                model.train()
+                    
+                scheduler.step()
 
