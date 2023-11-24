d23db135982652a4e409bd673efd1add748df33d
dongseop.kim <dongseop.kim@vuno.co>
2023-11-24 15:59:48 +0900

    wip

--- src/train_all.py
+++ src/train_all.py
@@ -7,10 +7,49 @@ from src.datamodules.datamodule import get_NIH_dataloader
 from src.models.resnet import NIHResNet
 
 
+_FORMAT_PER_ITER = '''
+epoch {num_epoch:03d}\titeration {num_iter:5d} {num_iter/len(loader_train)*100:.0f}\tloss: {loss.item():.4f}
+'''
 
-def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', device: int = 0, max_epoch: int = 100):
 
-    root_dir2 = "/home/ubuntu/miniforge3/etc/GOAT_41/GOAT_41"
+def forward_with_batch(batch: dict[str, torch.Tensor],
+                       model: nn.Module,
+                       device: str) -> tuple(torch.Tensor, torch.Tensor):
+    image: torch.Tensor = batch['image']
+    labels: torch.Tensor = batch['label']
+    image = image.to(device)
+    labels = labels.to(device)
+    logits = model(image)
+    return logits, labels
+
+
+def backward_with_batch(logits: torch.Tensor,
+                        labels: torch.Tensor,
+                        optimizer: torch.optim.Optimizer,
+                        criterion: torch.nn.Module) -> torch.Tensor:
+    optimizer.zero_grad()
+    loss: torch.Tensor = criterion(logits, labels.to(torch.float32))
+    loss.backward()
+    optimizer.step()
+    return loss
+
+
+def forward_backward_with_batch(batch: dict[str, torch.Tensor],
+                                model: nn.Module,
+                                optimizer: torch.optim.Optimizer,
+                                criterion: torch.nn.Module,
+                                device: str) -> tuple(torch.Tensor, torch.Tensor, torch.Tensor):
+    logits, labels = forward_with_batch(batch, model, device)
+    loss = backward_with_batch(logits, labels, optimizer, criterion)
+    return logits, labels, loss
+
+
+def train(root_dir: str,
+          batch_size: int = 4,
+          model_name: str = 'resnet50',
+          device: int = 0,
+          max_epoch: int = 100,
+          save_dir: str = None):
 
     # get NIH - dataset
     trainset = NIH(root_dir=root_dir, split='train',
@@ -18,7 +57,7 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
     testset = NIH(root_dir=root_dir, split='test',
                   image_size=512, image_channels=3)
     validset = NIH(root_dir=root_dir, split='val',
-                   image_size=512, image_channels=3,transform=None)
+                   image_size=512, image_channels=3)
 
     num_classes = trainset.num_classes
 
@@ -29,13 +68,11 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
     loader_test = get_NIH_dataloader(testset, batch_size=batch_size,
                                      use_basic=True)
 
-    # get model
-    model = NIHResNet(name=model_name, num_classs=10,
-                      out_indices=[4], return_logits=True)
-
     # device setting
     device = f'cuda:{device}'
-    model.to(device)
+    # get model
+    model = NIHResNet(name=model_name, num_classs=10,
+                      out_indices=[4], return_logits=True).to(device)
 
     criterion = torch.nn.BCEWithLogitsLoss(reduction='mean')
     optimizer = torch.optim.RAdam(model.parameters(), lr=0.0005)
@@ -43,61 +80,62 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
                                                      milestones=[50, 70],
                                                      gamma=0.1)
 
-    sens_per_lesion = [Recall(task='binary', num_classes=1).to(device)
-                       for _ in range(num_classes)]
-    spec_per_lesion = [Specificity(task='binary', num_classes=1).to(device)
-                       for _ in range(num_classes)]
-    
+    sens = Recall(task='binary', num_classes=1).to(device)
+    spec = Specificity(task='binary', num_classes=1).to(device)
+    sens_per_lesion = [sens.clone() for _ in range(num_classes)]
+    spec_per_lesion = [spec.clone() for _ in range(num_classes)]
 
+    # epoch
+    for num_epoch in range(max_epoch):
+        # set model to train mode
+        model.train()
+        model.return_logits = True
 
-    for num_epoch in range(max_epoch):  # epoch
+        # iteration
         for num_iter, batch in enumerate(loader_train):
-            image: torch.Tensor = batch['image']
-            labels: torch.Tensor = batch['label']
-            image = image.to(device)
-            labels = labels.to(device)
-
-            logits = model(image)
-
-            optimizer.zero_grad()
-
-            loss: torch.Tensor = criterion(logits, labels.to(torch.float32))
-            loss.backward()
-            optimizer.step()
-
+            # forward & backward with batch
+            logits, labels, loss = forward_backward_with_batch(batch, model,
+                                                               optimizer, criterion,
+                                                               device)
+            # evaluation with batch
             for idx_lesion in range(num_classes):
-                sens_per_lesion[idx_lesion].update(
-                    logits[:, idx_lesion], labels[:, idx_lesion])
-                spec_per_lesion[idx_lesion].update(
-                    logits[:, idx_lesion], labels[:, idx_lesion])
+                p, t = logits[:, idx_lesion], labels[:, idx_lesion]
+                sens_per_lesion[idx_lesion].update(p, t)
+                spec_per_lesion[idx_lesion].update(p, t)
 
-            computed_sens = [sens_per_lesion[idx_lesion].compute().item()
+            computed_sens = [sens_per_lesion[idx_lesion].compute()
                              for idx_lesion in range(num_classes)]
-            computed_spec = [spec_per_lesion[idx_lesion].compute().item()
+            computed_spec = [spec_per_lesion[idx_lesion].compute()
                              for idx_lesion in range(num_classes)]
 
             if num_iter % 10 == 0:
-                print(
-                    f'epoch {num_epoch:03d}\titeration {num_iter:5d} {num_iter/len(loader_train)*100:.0f}\tloss: {loss.item():.4f}')
-                print(["{:.6f}".format(sens) for sens in computed_sens])
-                print(["{:.6f}".format(spec) for spec in computed_spec])
+                print(_FORMAT_PER_ITER.format(num_epoch=num_epoch,
+                                              num_iter=num_iter,
+                                              loss=loss.item()))
+
+                print(["{:.4f}".format(sens.item()) for sens in computed_sens])
+                print(["{:.4f}".format(spec.item()) for spec in computed_spec])
+
+        # epoch end
+        for _sens, _spec in zip(sens_per_lesion, spec_per_lesion):
+            _sens.reset()
+            _spec.reset()
+
         model.eval()
+        model.return_logits = False
         best_accuracy = 0.0  # 초기 최고 정확도
         with torch.no_grad():
             correct = 0
             total = 0
             for num_iter, batch in enumerate(loader_valid):
-                image: torch.Tensor = batch['image']
-                labels: torch.Tensor = batch['label']
-                image = image.to(device)
-                labels = labels.to(device)
-                logits = model(image)
-                probs = torch.sigmoid(logits)  # Apply sigmoid to the logits
-                predicted = (probs.ge(0.4)).float()  # Generate predictions based on the threshold
+                probs, labels = forward_with_batch(batch, model, device)
+                # Generate predictions based on the threshold
+                predicted = (probs.ge(0.5)).float()
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-                print(f'Outputs for iteration {num_iter}: {probs}')  # Use probs instead of outputs
+                # Use probs instead of outputs
+                print(f'Outputs for iteration {num_iter}: {probs}')
 
                 for idx_lesion in range(num_classes):
                     sens_per_lesion[idx_lesion].update(
@@ -106,9 +144,9 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
                         probs[:, idx_lesion], labels[:, idx_lesion])  # Use probs instead of outputs
 
                 computed_sens = [sens_per_lesion[idx_lesion].compute().item()
-                                for idx_lesion in range(num_classes)]
+                                 for idx_lesion in range(num_classes)]
                 computed_spec = [spec_per_lesion[idx_lesion].compute().item()
-                                for idx_lesion in range(num_classes)]
+                                 for idx_lesion in range(num_classes)]
 
                 # Print additional information for each 10 iterations
                 if num_iter % 10 == 0:
@@ -124,12 +162,13 @@ def train(root_dir: str, batch_size: int = 4, model_name: str = 'resnet50', devi
             print(f'Accuracy on validation set: {accuracy:.4f}')
 
             # 모델 저장
+            save_dir = "/home/ubuntu/miniforge3/etc/GOAT_41/GOAT_41" if save_dir is None else save_dir
             if accuracy > best_accuracy:
                 best_accuracy = accuracy
-                model_path = os.path.join(root_dir2, 'best_model.pth')
+                model_path = os.path.join(save_dir, 'best_model.pth')
                 torch.save(model.state_dict(), model_path)
-                print('Best model saved with accuracy {:.4f}'.format(best_accuracy))
+                print('Best model saved with accuracy {:.4f}'.format(
+                    best_accuracy))
                 model.train()
-                    
-                scheduler.step()
 
+                scheduler.step()
